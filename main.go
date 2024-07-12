d97a367b7aeef45b3d4efa86001dd6f928497ee0
SahilSyed01 <sahilsyed7781@gmail.com>
2024-07-12 12:12:05 +0530

    A

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"api_testing/analyze"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Root directory not provided. Usage: go run main.go <rootDir>")
+	}
+
+	rootDir := os.Args[1]
+	collectionFile := "C:/Users/Relanto/Downloads/api_testing/api_testing/collection.json"
+	newmanReportPath := "C:/Users/Relanto/Desktop/T3/newman-report.json"
+
+	if err := analyze.RunNewman(collectionFile, newmanReportPath); err != nil {
+		log.Fatalf("Error running Newman: %v", err)
+	}
+
+	analyze.AnalyzeEndpoints(rootDir, newmanReportPath) //newman
+}
