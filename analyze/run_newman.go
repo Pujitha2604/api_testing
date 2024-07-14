9f0b445138785f6e40379567ec519cc31bb6c1b9
SahilSyed01 <sahilsyed7781@gmail.com>
2024-07-14 20:37:34 +0530

    4 test files

--- analyze/run_newman.go
+++ analyze/run_newman.go
@@ -1,15 +1,31 @@
 package analyze
 
 import (
-	"fmt"
 	"os/exec"
 )
 
+// CommandRunner defines the interface for running commands.
+type CommandRunner interface {
+	Run() error
+}
+
+// RealCommand implements the CommandRunner interface using exec.Cmd.
+type RealCommand struct {
+	cmd *exec.Cmd
+}
+
+// Run executes the command.
+func (c *RealCommand) Run() error {
+	return c.cmd.Run()
+}
+
+// RunNewman runs Newman with specified collection and report paths.
 func RunNewman(collectionFile string, reportPath string) error {
 	cmd := exec.Command("newman", "run", collectionFile, "--reporters", "json", "--reporter-json-export", reportPath)
-	err := cmd.Run()
-	if err != nil {
-		return fmt.Errorf("error running Newman: %v", err)
-	}
-	return err
+	return runCmd(cmd)
+}
+
+// Function to run command, can be overridden for testing
+var runCmd = func(cmd *exec.Cmd) error {
+	return cmd.Run()
 }
