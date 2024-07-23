ddd11439aa8c117676a1a814ee97bbd45b4d8dc7
SahilSyed01 <sahilsyed7781@gmail.com>
2024-07-23 18:25:27 +0530

    changes

--- main.go
+++ main.go
@@ -5,17 +5,10 @@ import (
 )
 
 func main() {
-	// if len(os.Args) < 2 {
-	// 	log.Fatal("Root directory not provided. Usage: go run main.go <rootDir>")
-	// }
 
 	rootDir := "/home/ec2-user/web-service"
-	//collectionFile := "C:/Users/Rekanto/Desktop/api_testing/collection.json"
-	newmanReportPath := "/home/ec2-user/web-service/collection/newman-report.json"
 
-	// if err := analyze.RunNewman(collectionFile, newmanReportPath); err != nil {
-	// 	log.Fatalf("Error running Newman: %v", err)
-	// }
+	newmanReportPath := "/home/ec2-user/web-service/collection/newman-report.json"
 
 	analyze.Analysis(rootDir, newmanReportPath)
 }
