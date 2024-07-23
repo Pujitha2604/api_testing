b54efa9762c373f4e0c2cb522a484bf60fd6cfbb
SahilSyed01 <sahilsyed7781@gmail.com>
2024-07-23 12:28:48 +0530

    ec2

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ 
+HOST_DIR="/home/T3"
+CONTAINER_DIR="/app/files"
+ 
+echo "Contents of the host directory:"
+ls -la "$HOST_DIR"
+ 
+docker run --rm -v "$HOST_DIR:$CONTAINER_DIR" -v /var/run/docker.sock:/var/run/docker.sock -w "$CONTAINER_DIR" test-tools /bin/bash -c "echo 'Contents of the container directory:' && ls -la $CONTAINER_DIR"
+ 
+docker run --rm -v "$HOST_DIR:$CONTAINER_DIR" -v /var/run/docker.sock:/var/run/docker.sock -w "$CONTAINER_DIR" test-tools /bin/bash -c "chmod +x apitests.sh && echo 'Running apitests.sh:' && ./apitests.sh"
