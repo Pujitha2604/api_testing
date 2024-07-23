1c5058aa3d8af72de368bc58bb281d1d9f01959a
SahilSyed01 <sahilsyed7781@gmail.com>
2024-07-23 18:37:17 +0530

    changes

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 
 	rootDir := "/home/ec2-user/web-service"
-
+	
 	newmanReportPath := "/home/ec2-user/web-service/collection/newman-report.json"
 
 	analyze.Analysis(rootDir, newmanReportPath)
